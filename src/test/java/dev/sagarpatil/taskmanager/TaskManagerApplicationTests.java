8265392a3c5ff59d96f931de4743393cc971214d
sagarpatil <sagarpatil0041@gmail.com>
2026-07-04 19:01:13 +0200

    feat: project skeleton from Spring Initializr

---
+++ src/test/java/dev/sagarpatil/taskmanager/TaskManagerApplicationTests.java
@@ -0,0 +1,15 @@
+package dev.sagarpatil.taskmanager;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.context.annotation.Import;
+
+@Import(TestcontainersConfiguration.class)
+@SpringBootTest
+class TaskManagerApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
